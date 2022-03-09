1a5055d4b3e636365458ebbde2698dd566823f0a
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2022-03-09 15:50:18 +0530

    Added : Move Negative elements to one side

---
+++ Arrays/negativeInOneSideOfArray.java
@@ -0,0 +1,21 @@
+public class negativeInOneSideOfArray {
+    public static void main(String[] args) {
+        int[] arr={-2,-1,2,4,-3,-4,-9,20,-21},narr=new int[arr.length];
+        int temp,j=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]<0){
+                narr[j]=arr[i];
+                j++;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>0){
+                narr[j]=arr[i];
+                j++;
+            }
+        }
+        for (int i = 0; i < narr.length; i++) {
+            System.out.print(narr[i] + " ");
+        }
+    }
+}
