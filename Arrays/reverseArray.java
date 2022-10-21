b881348855ac07c4366e89744d788fc03bc6222d
Mohd Kaleem Saqlaini <mksaqlaini95@gmail.com>
2022-10-22 00:24:05 +0530

    Update reverseArray.java

--- Arrays/reverseArray.java
+++ Arrays/reverseArray.java
@@ -3,8 +3,10 @@ public class reverseArray {
     public static void main(String[] args) {
 
         int[] arr={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        System.out.print("Before Reversing : ");
         printArray(arr);
         rArray(arr);
+        System.out.print("After Reversing : ");
         printArray(arr);
     }
 
