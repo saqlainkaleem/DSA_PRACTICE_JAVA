8c1d89b1cf707239a0750c0fb867eb72ae5b87cc
Mohd Kaleem Saqlaini <64285565+saqlainkaleem@users.noreply.github.com>
2024-06-24 23:30:53 +0530

    Create 995.java

---
+++ Arrays/LeetCode/995.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+    int n = nums.length;
+    int res = 0;
+    Queue<Integer> queue = new LinkedList<>();
+    
+    for (int i = 0; i < n; i++) {
+        while (!queue.isEmpty() && queue.peek() <= i - k) {
+            queue.poll();
+        }
+        if ((queue.size() + nums[i]) % 2 == 0) {
+            if (i + k > n) return -1;
+            res++;
+            queue.offer(i);
+        }
+    }
+    return res;
+    }
+}
